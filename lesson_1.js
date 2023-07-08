480a974d5bca7791254c4002e58b7cf39c3ec88d
UdodOleg <udodolegofznu@gmail.com>
2023-07-08 18:13:39 +0300

    .

---
+++ lesson_1.js
@@ -0,0 +1,32 @@
+a = [{
+    primitive_variables: { 
+        string: 'Hello World',
+        undef: undefined,
+        bool: true,
+        num: 25
+    },
+    linked_variables: {
+        object: {
+            status: true,
+            comment: '...'
+        },
+        array: [1,2,3,4]
+    },
+    comment: 'Разместил 4 примитивных типа и ссылочные типы переменных яваскрипт'
+}, {
+    primitive_variables: { 
+        string: 'Hello World2',
+        undef: undefined,
+        bool: true,
+        num: 26
+    },
+    linked_variables: {
+        object: {
+            status: true,
+            comment: '...2'
+        },
+        array: [1,2,3,4]
+    },
+    comment: 'Разместил 4(2) примитивных типа и ссылочные типы переменных яваскрипт'
+}];
+console.log(a[1]['linked_variables']['object']);
---
+++ lesson_2.js
@@ -0,0 +1,29 @@
+const post = {
+    title: 'MyPost',
+    likesQty: 5
+};
+const post2 = post;
+post2.likesQty = 6;
+console.log(post.likesQty); // 6
+const post3 = Object.assign({}, post); // если нет вложенных объектов
+post3.likesQty = 7;
+console.log(post.likesQty); // 6
+console.log(post3.likesQty); // 7
+
+const person = {
+    name: "Oleg",
+    age: 19,
+    contact: {
+        phone: '+380669275418',
+        city: 'Zaporizhya',
+        country: 'Ukraine'
+    }
+}
+// const person1 = Object.assign({},person);
+// person1.contact.city='Kyiv';
+// console.log(person.contact.city); // изменится
+
+// const person2 = {...person}; // ссылки на вложенные объекты сохраняются
+const person2 = JSON.parse(JSON.stringify(person));
+person2.contact.city='Kyiv';
+console.log(person.contact.city);
